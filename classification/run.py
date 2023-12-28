969be8f1411a19f8ed56f1542fe3d60fca38a33d
ivan-dev-lab <roalivan4@gmail.com>
2023-12-28 19:55:40 +0500

    написан код для обучения моделей keras

--- classification/run.py
+++ classification/run.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 import re
+import numpy as np
+import tensorflow as tf
 from src.keras_model import create_model
 
 # data_path - путь к уже предобработанным данным
@@ -12,7 +14,7 @@ from src.keras_model import create_model
 # структура словаря models: список с моделью/моделями keras 'keras_models', список путей для сохранения - 'keras_models_path'
 # функция возвращает результаты обучения моделей
 def train (data_path: str, models: dict,  limit=None) -> dict:
-    models_results = {'sklearn_results': list, }
+    models_results = {'sklearn_results': [], 'keras_results': []}
     if limit: preprocessed_data = pd.read_csv(data_path, index_col=[0]).iloc[0:limit]
     else: preprocessed_data = pd.read_csv(data_path, index_col=[0])
     
@@ -29,8 +31,6 @@ def train (data_path: str, models: dict,  limit=None) -> dict:
     vect_x_test = vectorizer.transform(raw_documents=x_test)
 
     sklearn_results = []
-    print(vect_x_train.shape, y_train.shape, vect_x_test.shape, y_test.shape)
-
     if models.get('sklearn_models') != None:
         sklearn_models = models['sklearn_models']
         sklearn_models_path = models['sklearn_models_path']
@@ -59,17 +59,46 @@ def train (data_path: str, models: dict,  limit=None) -> dict:
 
             models_results['sklearn_results'] = sklearn_results
 
+    keras_results = []
     if models.get('keras_models') != None:
         keras_models = models['keras_models'] 
         keras_models_path = models['keras_models_path'] 
         if len(keras_models) > 0 and len(keras_models) == len(keras_models_path):
+
+            # vect_x_train, vect_x_test - разреженные представления данных, поэтому сначала их надо представить в виде плотных данных, затем в виде SparseTensor, а затем применить к ним tf.sparse.reorder
+            vect_x_train_dense = vect_x_train.toarray()
+            vect_x_train_sparse = tf.sparse.from_dense(vect_x_train_dense)
+            vect_x_train_reordered = tf.sparse.reorder(vect_x_train_sparse)
+
+            vect_x_test_dense = vect_x_test.toarray()
+            vect_x_test_sparse = tf.sparse.from_dense(vect_x_test_dense)
+            vect_x_test_reordered = tf.sparse.reorder(vect_x_test_sparse)
+
+            y_train = np.array(y_train)
+            y_test = np.array(y_test)
+
             input_shape = (vect_x_test.shape[1], y_train.shape[1])
-            for keras_model in keras_models:
+
+            for index, keras_model in enumerate(keras_models):
+                current_result = {'model': 'str', 'accuracy': float}
+
+                model_name = ''.join([match.group()[1:len(match.group())-1] for match in re.finditer(r"/[A-Z]\w*.", keras_models_path[index])])
+                
+                current_result['model'] = model_name
+
+                # keras_model - функция создания модели keras
                 model = keras_model(input_shape)
 
+                model.fit(x=vect_x_train_reordered, y=y_train, batch_size=32, epochs=4)
+                accuracy = model.evaluate(x=vect_x_test_reordered, y=y_test, batch_size=32)[1]
+
+                current_result['accuracy'] = accuracy
                 
-                model.fit(x=vect_x_train, y=y_train, batch_size=32, epochs=10)
-                # BUG: ValueError: Failed to find data adapter that can handle input: <class 'NoneType'>, <class 'NoneType'>
+                keras_results.append(current_result)
+
+                with open(keras_models_path[index], mode='wb+') as model_file: pickle.dump(model, model_file)
+
+            models_results['keras_results'] = keras_results
 
     with open('classification/models/TF-IDF.pkl', mode='wb+') as vectorizer_file: 
         pickle.dump(vectorizer, vectorizer_file)
@@ -78,9 +107,16 @@ def train (data_path: str, models: dict,  limit=None) -> dict:
 
 
 keras_models = [create_model]
-keras_models_path = ['classification/models/keras_models/model.h5']
+keras_models_path = ['classification/models/keras_models/FullyConnected_Keras.pkl']
 models = {}
 models['keras_models'] = keras_models
 models['keras_models_path'] = keras_models_path
 
-train('data/preprocessed_docs.csv', models, limit=10)
+results = train('data/preprocessed_docs.csv', models, limit=None)
+print(results)
+
+# TODO: сделать обучение моделей ассинхронным
+# TODO: написать коллбеки для моделей keras
+# TODO: написать подбор параметров GridSearchCV
+# TODO: написать другие архитектуры для моделей keras
+# TODO: написать модель на pytorch
