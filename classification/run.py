6a0cc09916e3cf969c1ee271f938b2c8c77faea3
ivan-dev-lab <roalivan4@gmail.com>
2023-12-28 17:44:56 +0500

    начата работа над обучением моделей keras

--- classification/run.py
+++ classification/run.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 import re
+from src.keras_model import create_model
 
 # data_path - путь к уже предобработанным данным
 # models - словарь, содержащий функции вызова sklearn моделей и функцию создания модели keras 
@@ -28,34 +29,58 @@ def train (data_path: str, models: dict,  limit=None) -> dict:
     vect_x_test = vectorizer.transform(raw_documents=x_test)
 
     sklearn_results = []
-    sklearn_models = models['sklearn_models']
-    sklearn_models_path = models['sklearn_models_path']
-    
-    if len(sklearn_models) > 0 and len(sklearn_models) == len(sklearn_models_path):
-        for index, model in enumerate(sklearn_models):
-            current_result = {'model': str, 'accuracy': float}
+    print(vect_x_train.shape, y_train.shape, vect_x_test.shape, y_test.shape)
 
-            # регулярное выражение для получения названия модели из пути для сохранения
-            # 'models/DecisionTreeClassifier.pkl' -> DecisionTreeClassifier
-            model_name = ''.join([match.group()[1:len(match.group())-1] for match in re.finditer(r"/[A-Z]\w*.", sklearn_models_path[index])])
+    if models.get('sklearn_models') != None:
+        sklearn_models = models['sklearn_models']
+        sklearn_models_path = models['sklearn_models_path']
+        
+        if len(sklearn_models) > 0 and len(sklearn_models) == len(sklearn_models_path):
+            for index, model in enumerate(sklearn_models):
+                current_result = {'model': str, 'accuracy': float}
 
-            current_result['model'] = model_name
+                # регулярное выражение для получения названия модели из пути для сохранения
+                # 'models/DecisionTreeClassifier.pkl' -> DecisionTreeClassifier
+                model_name = ''.join([match.group()[1:len(match.group())-1] for match in re.finditer(r"/[A-Z]\w*.", sklearn_models_path[index])])
 
-            model = sklearn_models[index]()
-            model.fit(vect_x_train, y_train)
+                current_result['model'] = model_name
 
-            y_pred = model.predict(vect_x_test)
-            score = accuracy_score(y_test, y_pred)
-            current_result['accuracy'] = score
+                model = sklearn_models[index]()
+                model.fit(vect_x_train, y_train)
 
-            sklearn_results.append(current_result)
+                y_pred = model.predict(vect_x_test)
+                score = accuracy_score(y_test, y_pred)
+                current_result['accuracy'] = score
 
-            with open(sklearn_models_path[index], mode='wb+') as model_file: 
-                pickle.dump(model, model_file)
+                sklearn_results.append(current_result)
 
-        models_results['sklearn_results'] = sklearn_results
+                with open(sklearn_models_path[index], mode='wb+') as model_file: 
+                    pickle.dump(model, model_file)
+
+            models_results['sklearn_results'] = sklearn_results
+
+    if models.get('keras_models') != None:
+        keras_models = models['keras_models'] 
+        keras_models_path = models['keras_models_path'] 
+        if len(keras_models) > 0 and len(keras_models) == len(keras_models_path):
+            input_shape = (vect_x_test.shape[1], y_train.shape[1])
+            for keras_model in keras_models:
+                model = keras_model(input_shape)
+
+                
+                model.fit(x=vect_x_train, y=y_train, batch_size=32, epochs=10)
+                # BUG: ValueError: Failed to find data adapter that can handle input: <class 'NoneType'>, <class 'NoneType'>
 
     with open('classification/models/TF-IDF.pkl', mode='wb+') as vectorizer_file: 
         pickle.dump(vectorizer, vectorizer_file)
     
     return models_results
+
+
+keras_models = [create_model]
+keras_models_path = ['classification/models/keras_models/model.h5']
+models = {}
+models['keras_models'] = keras_models
+models['keras_models_path'] = keras_models_path
+
+train('data/preprocessed_docs.csv', models, limit=10)
