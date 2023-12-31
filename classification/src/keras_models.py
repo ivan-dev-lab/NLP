8c16dbadec3bbe57dbe0b420217e214703573f2d
ivan-dev-lab <roalivan4@gmail.com>
2023-12-31 13:45:58 +0500

    внесены изменения в архитектуру модели

--- classification/src/keras_models.py
+++ classification/src/keras_models.py
@@ -3,15 +3,16 @@ import tensorflow as tf
 def FullyConnected_Keras (input_shape: tuple):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Input(shape=((input_shape[0],))))
-    model.add(tf.keras.layers.Dense(units=512, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
     model.add(tf.keras.layers.Dropout(rate=0.4))
     model.add(tf.keras.layers.Dense(units=256, activation='relu'))
     model.add(tf.keras.layers.Dropout(rate=0.4))
     model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
     model.add(tf.keras.layers.Dense(units=input_shape[1], activation='softmax'))
 
     model.compile(
-        optimizer = 'adam',
+        optimizer = tf.keras.optimizers.Adam(0.001),
         loss = 'categorical_crossentropy',
         metrics = ['accuracy']
     )
